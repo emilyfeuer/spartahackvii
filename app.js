42dbfba1ebcb48456b133ba98a793376dad1aa00
ZaneMODell <94186155+ZaneMODell@users.noreply.github.com>
2022-04-09 15:50:42 -0400

    Updated HTML and Working on Javascript itinerary list

--- app.js
+++ app.js
@@ -30,21 +30,45 @@ var bed_time;
 
 
 function assignPreferences(){
-  var userInput = document.getElementById('dd').value;
-  alert(userInput)}
+  city_travelled = some shit idk;
 
 
+// name, open_time, close_time, cost_scale, radius, local
 // let Udon_Sushi_Restaurant = new Attraction("Udon_Sushi_Restaurant", 12, 20, 3, 3, true)
 // let Omi_Sushi = new Attraction("Omi_Sushi", 11, 20, 2, 1, true)
 // let East_Cafe = new Attraction("East_Cafe", 5, 24, 2, 3, true)
 // let Royal_Pot = new Attraction("Royal_Pot", 10, 24, 2, 2, true)
+
+//AMERICAN
+let Olympic_Broil = new Attraction("Olympic Broil", 11, 20, 1, 6.2, true)
 let Buffalo_Wild_Wings = new Attraction("Buffalo_Wild_Wings", 11, 24, 2, 1, false)
 let iHop = new Attraction("iHop", 7, 15, 2, 1, false)
-let 
+let Red_Haven = new Attraction("Red Haven", 17, 20, 3, 2.3, true)
+
+//ITALIAN
+let Sidebar = new Attraction("Sidebar", 12, 0, 1, 2.5, true)
+let Jets_Pizza = new Attraction("Jet's Pizza", 10, 22, 2, 2.2, false)
+let Andiamo_Fenton = new Attraction("Andiamo Fenton", 15, 22, 3, 60, true)
+
+//Indoor Activities
+let Spare_Time = new Attraction("Spare Time", 13, 23, 2, 2.2, true)
+
+//Outdoor Activities
+let Horticulture_Gardens = new Attraction("Horticulture Gardens", 6, 22, 1, 1, true)
 
 
 var data_dict = {
-"cusine": {"Japanese": [Udon_Sushi_Restaurant, Omi_Sushi], "Chinese": [East_Cafe, Royal_Pot], "American": [Buffalo_Wild_Wings, iHop]}, 
-"sightseeing": {}
+"Food": {"American": [Olympic_Broil, Buffalo_Wild_Wings, iHop, Red_Haven], "Italian": [Sidebar, Jets_Pizza, Andiamo_Fenton]}, 
+"Indoor": {Spare_Time}, "Outdoor": {Horticulture_Gardens}
+}
+
+
+var itinerary_list = []
+
+for (let key = 0; key < length(data_dict); key++){
+  var value = data_dict[key];
+  for (let activity = 0; activity < length(value); activity++){
+    
+  }
 }
 
