737cb998eec283870b400f8aa5834b06bae8b9d1
emilyfeuer <emilymfeuer@gmail.com>
2022-04-10 05:36:43 -0400

    added to about.html and purpose.html

--- spartahackvii-main/app.js
+++ spartahackvii-main/app.js
@@ -13,7 +13,7 @@ class Attraction {
 
 //Preference variables
 
-var city_travelled;
+var city_traveled;
 var begin_date;
 var end_date;
 var activity_level;
@@ -31,59 +31,90 @@ var bed_time;
 
 
 function assignPreferences(){
+  var location = document.getElementById("location")
+  var budget = document.getElementById("budget")
+  var begin_date = document.getElementById("begin_date")
+  var end_date = document.getElementById("end_date")
+  var hotel = document.getElementById("hotel")
+  var radius = document.getElementById("radius")
+  var food1 = document.getElementById("food1") 
+  var food2 = document.getElementById("food2")
+  var num_people = document.getElementById("num_people")
+  var begin_time = document.getElementById("begin_time")
+  var end_time = document.getElementById("end_time")
 
 }
 
-// name, open_time, close_time, cost_scale, radius, local
-// let Udon_Sushi_Restaurant = new Attraction("Udon_Sushi_Restaurant", 12, 20, 3, 3, true)
-// let Omi_Sushi = new Attraction("Omi_Sushi", 11, 20, 2, 1, true)
-// let East_Cafe = new Attraction("East_Cafe", 5, 24, 2, 3, true)
-// let Royal_Pot = new Attraction("Royal_Pot", 10, 24, 2, 2, true)
+function createDatabase(){
+  // name, open_time, close_time, cost_scale, radius, local
+  // let Udon_Sushi_Restaurant = new Attraction("Udon_Sushi_Restaurant", 12, 20, 3, 3, true)
+  // let Omi_Sushi = new Attraction("Omi_Sushi", 11, 20, 2, 1, true)
+  // let East_Cafe = new Attraction("East_Cafe", 5, 24, 2, 3, true)
+  // let Royal_Pot = new Attraction("Royal_Pot", 10, 24, 2, 2, true)
 
-//AMERICAN
-let Olympic_Broil = new Attraction("Olympic Broil", 11, 20, 1, 6.2, true)
-let Buffalo_Wild_Wings = new Attraction("Buffalo_Wild_Wings", 11, 24, 2, 1, false)
-let iHop = new Attraction("iHop", 7, 15, 2, 1, false)
-let Red_Haven = new Attraction("Red Haven", 17, 20, 3, 2.3, true)
+  //AMERICAN
+  let Olympic_Broil = new Attraction("Olympic Broil", 11, 20, 1, 6.2, true)
+  let Buffalo_Wild_Wings = new Attraction("Buffalo_Wild_Wings", 11, 24, 2, 1, false)
+  let iHop = new Attraction("iHop", 7, 15, 2, 1, false)
+  let Red_Haven = new Attraction("Red Haven", 17, 20, 3, 2.3, true)
 
-//ITALIAN
-let Sidebar = new Attraction("Sidebar", 12, 0, 1, 2.5, true)
-let Jets_Pizza = new Attraction("Jet's Pizza", 10, 22, 2, 2.2, false)
-let Andiamo_Fenton = new Attraction("Andiamo Fenton", 15, 22, 3, 60, true)
+  //ITALIAN
+  let Sidebar = new Attraction("Sidebar", 12, 0, 1, 2.5, true)
+  let Jets_Pizza = new Attraction("Jet's Pizza", 10, 22, 2, 2.2, false)
+  let Andiamo_Fenton = new Attraction("Andiamo Fenton", 15, 22, 3, 60, true)
 
-//Indoor Activities
-let Spare_Time = new Attraction("Spare Time", 13, 23, 2, 2.2, true)
+  //Indoor Activities
+  let Spare_Time = new Attraction("Spare Time", 13, 23, 2, 2.2, true)
 
-//Outdoor Activities
-let Horticulture_Gardens = new Attraction("Horticulture Gardens", 6, 22, 1, 1, true)
-
-
-var data_dict = {
-"Food": {"American": [Olympic_Broil, Buffalo_Wild_Wings, iHop, Red_Haven], "Italian": [Sidebar, Jets_Pizza, Andiamo_Fenton]}, 
-"Indoor": {Spare_Time}, "Outdoor": {Horticulture_Gardens}
-}
+  //Outdoor Activities
+  let Horticulture_Gardens = new Attraction("Horticulture Gardens", 6, 22, 1, 1, true)
 
 
-var itinerary_list = [];
-
-for (let key = 0; key < length(data_dict); key++){
-  var add_count;
-  var activity_list = data_dict[key];
-  for (let i = 0; i < length(value); i++){
-    if ((activity_list)[i].activity_level <= activity_level){
-      add_count += 1;
-    }
-    if ((activity_list)[i].radius <= preferred_radius){
-      add_count += 1;
-    }
-    if ((activity_list)[i].cost_scale <= budget_range){
-      add_count += 1;
-    }
-    if (activity_list.key in food_preferences){
-      add_count += 1;
-    }
+  var data_dict = {
+  "Food": {"American": [Olympic_Broil, Buffalo_Wild_Wings, iHop, Red_Haven], "Italian": [Sidebar, Jets_Pizza, Andiamo_Fenton]}, 
+  "Activities": {"Indoor": [Spare_Time], "Outdoor": [Horticulture_Gardens]}
   }
+
+
+  var itinerary_list = [];
+
+  for (const [key, value] of Object.entries(data_dict)) {
+    var add_count;
+    var activity_list = data_dict[key];
+    for (const [inner_key, inner_value] of Object.entries(value)){
+      for (var i = 0; i < length(inner_value); i++){
+        if (inner_value[i].activity_level <= activity_level){
+          add_count += 1;
+        }
+        if (inner_value[i].radius <= preferred_radius){
+          add_count += 1;
+        }
+        if (inner_value[i].cost_scale <= budget_range){
+          add_count += 1;
+        }
+        if (inner_key in food_preferences){
+          add_count += 1;
+        }
+
+        if (add_count >= 2){
+          itinerary_list.push(inner_value)
+        }
+        add_count = 0;
+      }
+    }
+
+  }
+  return itinerary_list;
 }
 
+function createItinerary(){
 
+}
 
+function submitFunction(){
+  assignPreferences();
+
+  createDatabase();
+
+  
+}
